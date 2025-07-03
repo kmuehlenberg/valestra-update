b77b21baa3aba8d2ffbca19a8153d96b878b73f4
kmuehlenberg <muehlenbergz55@gmail.com>
2025-07-02 22:24:43 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: ['valestra.co', 'your-image-cdn.com'], // update with any domains you serve images from
-  },
-  trailingSlash: false,
+  swcMinify: true,
 };
 
 module.exports = nextConfig;
